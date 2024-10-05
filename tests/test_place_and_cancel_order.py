cc8daa6eb1b9688609564a21cbb7cbef30f9bf67
Izem0 <izem.mangione@gmail.com>
2024-10-05 18:58:07 +0200

    skip test_place_and_cancel_order() for now

--- tests/test_place_and_cancel_order.py
+++ tests/test_place_and_cancel_order.py
@@ -2,6 +2,8 @@ import sys
 
 import pytest
 
+pytestmark = pytest.mark.skip("skip this test for now")
+
 # cannot currently run tests on github runners (because hosted in the US)
 pytestmark = pytest.mark.skipif(
     not sys.platform.startswith("win"), reason="tests for windows only"
@@ -15,8 +17,13 @@ def test_place_and_cancel_order(exchanges):
         assert isinstance(orders, list)
 
         # place order
+        market = "XRPUSDC"
         order = exc.place_order(
-            "ETHUSDT", type="limit", side="buy", qty=0.01, price=1000
+            market,
+            type="limit",
+            side="buy",
+            qty=15,
+            price=round(exc.get_market_price(market) * 0.95, 2),
         )
         assert order.get("id") is not None
 
